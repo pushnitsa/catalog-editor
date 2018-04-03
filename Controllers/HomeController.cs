341c6c73a0da7e38bcdcc1b643621396b5524dd8
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-04-03 21:07:03 +0200

    Add home controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogEditor.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+            
+        }
+
+        [HttpGet("/")]
+        public async void Index()
+        {
+            
+        }
+    }
+}
