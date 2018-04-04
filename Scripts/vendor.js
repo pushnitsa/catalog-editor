a459bd5adc2ff7f6e5285c056518aca970b41cf9
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-04-05 01:45:18 +0200

    Add angular treeview module

--- Scripts/vendor.js
+++ Scripts/vendor.js
@@ -1,3 +1,4 @@
 //= ../node_modules/jquery/dist/jquery.min.js
 //= ../node_modules/angular/angular.min.js
 //= ../node_modules/bootstrap/dist/js/bootstrap.min.js
+//= ../node_modules/angularTreeview/angular.treeview.min.js
