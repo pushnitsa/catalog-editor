bc61e89a86d7675be7cb3cc0a2cee1bb2f9c1747
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-04-05 21:52:28 +0200

    Add Product DTO

---
+++ DTO/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace CatalogEditor.DTO
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string Image { get; set; }
+        
+        public int Quantity { get; set; }
+        
+        public decimal Price { get; set; }
+    }
+}
