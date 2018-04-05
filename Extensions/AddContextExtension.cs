ac87a377f3545d8a5f6f3fa292a71a4bcce8f08d
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-04-05 19:02:38 +0200

    Add context extension

---
+++ Extensions/AddContextExtension.cs
@@ -0,0 +1,19 @@
+﻿using CatalogEditor.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class AddContextExtension
+    {
+        public static IServiceCollection AddContext(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection
+                    .AddTransient(
+                        s => new DbContextOptionsBuilder()
+                            .UseInMemoryDatabase("CatalogDb")
+                            .Options
+                    )
+                    .AddTransient<IContextFactory, ContextFactory>();
+        }
+    }
+}
