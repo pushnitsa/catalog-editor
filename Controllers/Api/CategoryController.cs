fff5f723c7ac564c26f284bc28e9c6c80dc64486
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-04-05 19:02:52 +0200

    Add api controllers

---
+++ Controllers/Api/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogEditor.Controllers.Api
+{
+    [Route("api/category")]
+    public class CategoryController : Controller
+    {
+
+        public CategoryController()
+        {
+            
+        }
+
+        [HttpGet("get")]
+        public async Task<IActionResult> GetCategories()
+        {
+            return new ObjectResult("");
+        }
+    }
+}
---
+++ Controllers/Api/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using CatalogEditor.DatabaseContext;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogEditor.Controllers.Api
+{
+    [Route("api/product")]
+    public class ProductController : Controller
+    {
+        private readonly IContextFactory _contextFactory;
+        
+        public ProductController(IContextFactory contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+        [HttpGet("get/{categoryId}")]
+        public async Task GetProducts(int categoryId)
+        {
+            using (var context = _contextFactory.Create())
+            {
+                var productList = await context.Products.ToListAsync();
+            }
+        }
+    }
+}
