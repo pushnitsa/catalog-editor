0509abe3358060e8658adfa1b1abf683f8bc496f
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-04-05 19:01:35 +0200

    Add models

---
+++ Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace CatalogEditor.Models
+{
+    public class Category
+    {
+
+        public Category()
+        {
+            Childs = new List<Category>();
+        }
+        
+        public int Id { get; private set; }
+        
+        public string Name { get; set; }
+        
+        public ICollection<Category> Childs { get; set; }
+        
+        public int? CategoryId { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace CatalogEditor.Models
+{
+    public class Product
+    {
+        
+        public int Id { get; private set; }
+        
+        public string Name { get; set; }
+        
+        public int Quantity { get; set; }
+        
+        public decimal Price { get; set; }
+        
+        public Category Category { get; set; }
+    }
+}
