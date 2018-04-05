3a865c049e5a1c74dd2cac9b811825e7f8d5f019
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-04-05 21:53:40 +0200

    Modify Product controller

--- Controllers/Api/ProductController.cs
+++ Controllers/Api/ProductController.cs
@@ -1,27 +1,53 @@
 ﻿using System.Threading.Tasks;
-using CatalogEditor.DatabaseContext;
+using CatalogEditor.Deserializer;
+using CatalogEditor.Processor;
+using CatalogEditor.Serializer;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace CatalogEditor.Controllers.Api
 {
     [Route("api/product")]
     public class ProductController : Controller
     {
-        private readonly IContextFactory _contextFactory;
+        private readonly ProductProcessor _productProcessor;
+        private readonly IEntitySerializer _serializer;
+        private readonly IEntityDeserializer _deserializer;
         
-        public ProductController(IContextFactory contextFactory)
+        public ProductController(
+            ProductProcessor productProcessor, 
+            IEntitySerializer serializer,
+            IEntityDeserializer deserializer
+        )
         {
-            _contextFactory = contextFactory;
+            _productProcessor = productProcessor;
+            _serializer = serializer;
+            _deserializer = deserializer;
         }
 
         [HttpGet("get/{categoryId}")]
-        public async Task GetProducts(int categoryId)
+        public async Task<IActionResult> GetProducts(int categoryId)
         {
-            using (var context = _contextFactory.Create())
-            {
-                var productList = await context.Products.ToListAsync();
-            }
+            var products = await _productProcessor.GetProductsAsync(categoryId);
+            
+            return new ObjectResult(_serializer.Serialize(products));
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteProducts()
+        {
+            return new ObjectResult(true);
+        }
+
+        [HttpPut()]
+        public IActionResult UpdateProduct()
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct()
+        {
+            return new ObjectResult("");
         }
     }
 }
