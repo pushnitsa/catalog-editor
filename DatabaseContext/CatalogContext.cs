886031fc133d906ff892364286b22745887a0965
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-04-05 19:01:26 +0200

    Add database context

---
+++ DatabaseContext/CatalogContext.cs
@@ -0,0 +1,28 @@
+﻿using CatalogEditor.Models;
+using CatalogEditor.Models.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogEditor.DatabaseContext
+{
+    public class CatalogContext : DbContext
+    {
+
+        public CatalogContext(DbContextOptions options)
+        :base(options)
+        {
+            
+        }
+        
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder
+                .ApplyConfiguration(new ProductConfiguration())
+                .ApplyConfiguration(new CategoryConfiguration());
+        } 
+    }
+}
