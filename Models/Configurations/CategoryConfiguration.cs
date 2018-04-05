655d6972e3d368ea20f01e387d34e62a94239aaf
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-04-05 19:02:05 +0200

    Add models configurations

---
+++ Models/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CatalogEditor.Models.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(e => e.Name).IsRequired();
+
+            builder.Property(e => e.CategoryId).HasColumnName("ParentId");
+        }
+    }
+}
---
+++ Models/Configurations/ProductConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CatalogEditor.Models.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(e => e.Name).IsRequired();
+        }
+    }
+}
