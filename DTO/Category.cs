2b9259804b69fc14a77b74823dcf23e2b899f505
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-04-05 21:04:15 +0200

    Category DTO

--- DTO/Category.cs
+++ DTO/Category.cs
@@ -1,7 +1,13 @@
-﻿namespace CatalogEditor.DTO
+﻿using System.Collections.Generic;
+
+namespace CatalogEditor.DTO
 {
     public class Category
     {
+        public int Id { get; set; }
         
+        public string Name { get; set; }
+        
+        public List<Category> Childs { get; set; }
     }
 }
